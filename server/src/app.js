cce6dba2cd52a45fd623ac33df08ae317e936e62
nattapon kumyaito <nattaponk@nu.ac.th>
2025-12-12 09:23:07 +0700

    ch5

--- server/src/app.js
+++ server/src/app.js
@@ -1,27 +1,14 @@
 let express = require('express');
+let bodyParser = require('body-parser')
+
 const app = express();
 
-app.get('/status', (req, res) => {
-    res.send('Hello Node.js Server!')
-});
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/hello/:person', function (req, res) {
-    console.log('hello - ' + req.params.person)
-    res.send('say hello with ' + req.params.person)
-});
+// เรียกใช้ routes โดยส่ง app เข้าไป
+require('./routes')(app)
 
-// get user by id
-app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน')
-})
-// get all user
-app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูลผู้ใช้งาน')
-})
-// get all user
-app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
-})
 let port = process.env.PORT || 8081;
 
 app.listen(port, () => {
